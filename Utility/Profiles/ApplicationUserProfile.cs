ef73daafab2b66a85340faa36b2e5dddc02b234b
Mohamed abdo <159048705+Mohamedabdo273@users.noreply.github.com>
2025-01-24 14:50:36 +0200

    Add files via upload

---
+++ Utility/Profiles/ApplicationUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WebsitSellsLaptop.DTO;
+using WebsitSellsLaptop.Models;
+
+public class ApplicationUserProfile : Profile
+{
+    public ApplicationUserProfile()
+    {
+        CreateMap<ApplicationUserDto, ApplicationUser>()
+            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.FirstName + src.LastName))
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+
+    }
+}
